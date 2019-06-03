9d5ba5e30b13f379673d492b59d3c61cd671fb36
Chasmer <36886220+ThatrealChasmer@users.noreply.github.com>
2019-06-03 21:34:23 +0200

    color controller

---
+++ NAI Projekt/Assets/Scripts/ColorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SharpNeat.Phenomes;
+using UnityEngine;
+
+public class ColorController : UnitController
+{
+    public Color color;
+    private bool isRunning;
+    private IBlackBox box;
+
+    public float fitness;
+
+    public int red;
+    public int green;
+    public int blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = gameObject.GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Activate(IBlackBox box)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override float GetFitness()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Stop()
+    {
+        throw new System.NotImplementedException();
+    }
+}
